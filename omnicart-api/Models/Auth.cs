2bda11cfb2c3c76992b832c94b37e6c6022749d3
ManeeshDev <maneeshprashanth@gmail.com>
2024-10-01 02:56:11 +0530

    Create Auth model for auth related class types

---
+++ omnicart-api/Models/Auth.cs
@@ -0,0 +1,37 @@
+namespace omnicart_api.Models
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RegisterRequest
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class ForgotPasswordRequest
+    {
+        public string Email { get; set; }
+    }
+
+    public class ResetPasswordRequest
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string NewPassword { get; set; }
+    }
+
+    public class ChangePasswordRequest
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+
+}
+
+
